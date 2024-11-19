1274739c31a2d60878ca2775fa198fb312e0ab28
DavyRoy <125256077+DavyRoy@users.noreply.github.com>
2024-11-19 14:33:58 +0300

    variables.tf

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -1,14 +1,15 @@
 ###cloud vars
+#variable "token" {
+#  type        = string
+#  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+#}
 
 
-variable "cloud_id" {
-  type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
-}
-
-variable "folder_id" {
-  type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+variable "id" {
+  type = object({
+    cloud_id  = string
+    folder_id = string
+  })
 }
 
 variable "default_zone" {
@@ -28,11 +29,73 @@ variable "vpc_name" {
   description = "VPC network & subnet name"
 }
 
+variable "vm_web_image" {
+  type    = string
+  default = "ubuntu-2004-lts"
+}
+
+variable "env" {
+  type    = string
+  default = "develop"
+}
+
+variable "project" {
+  type    = string
+  default = "platform"
+}
+
+variable "role" {
+  type    = string
+  default = "web"
+}
+
+
+variable "metadata" {
+  type = object({
+    serial-port-enable = number
+    ssh-keys           = string
+  })
+}
+
+
+variable "vms_resources" {
+  type = map(object({
+    cores         = number
+    memory        = number
+    core_fraction = number
+  }))
+}
+
+variable "vm_web_resource" {
+  type = list(object({
+    #instance_name = string
+    #instance_cores         = number
+    #instance_memory        = number
+    #instance_core_fraction = number
+    public_ip   = bool
+    platform    = string
+    preemptible = bool
+  }))
+  default = [
+    {
+      #instance_name = "netology-develop-platform-web"
+      #instance_cores         = 2
+      #instance_memory        = 1
+      #instance_core_fraction = 5
+      public_ip   = true
+      platform    = "standard-v1"
+      preemptible = true
+
+    }
+  ]
+}
 
 ###ssh vars
 
+/*
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
+  default     = "...."
   description = "ssh-keygen -t ed25519"
 }
+*/
